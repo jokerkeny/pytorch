3ba3420a680a00f6dd413849bcb2990d5df49263
iurii zdebskyi <47012416+izdeby@users.noreply.github.com>
2019-10-21 15:54:08 -0400

    Lint

--- test/test_torch.py
+++ test/test_torch.py
@@ -5142,7 +5142,7 @@ tensor([[[1., 1., 1.,  ..., 1., 1., 1.],
             model = torch.nn.Conv2d(1, 1, (1, 3), stride=1, padding=0, bias=False)
             model.weight.data = weight
             out = model(input)
-    
+
     def test_tensor_from_sequence(self):
         class MockSequence(object):
             def __init__(self, lst):
@@ -10027,7 +10027,7 @@ class TestTorchDeviceType(TestCase):
             if dt == torch.bfloat16:
                 self.assertRaises(RuntimeError, lambda: torch.randint(5, (0, 1, 3, 0), dtype=dt, device=device))
                 continue
-            
+
             if dt == torch.half and device == 'cpu':
                 # fix once random is implemented for Half on CPU
                 self.assertRaises(RuntimeError, lambda: torch.randint(5, (0, 1, 3, 0), dtype=dt, device=device))
@@ -10105,7 +10105,6 @@ class TestTorchDeviceType(TestCase):
                 self.assertEqual(shape, torch.empty(shape, device=device, dtype=dt).shape)
                 self.assertEqual(shape, torch.empty_like(torch.zeros(shape, device=device, dtype=dt)).shape)
                 self.assertEqual(shape, torch.empty_strided(shape, (0,) * len(shape), device=device, dtype=dt).shape)
-                    
 
                 if dt == torch.half and device == "cpu":
                     # update once random is implemented for half on CPU
